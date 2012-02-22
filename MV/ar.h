d0e88e86b97ef961f38b1c691425f649e1cb878b
Pedro Costa <pdrcosta90@gmail.com>
2012-02-22 00:03:48 +0000

    Documented

--- MV/ar.h
+++ MV/ar.h
@@ -8,14 +8,36 @@ namespace cg
 	{
 		namespace ar
 		{
-			// Initializes AR Toolkit
-			void init();
+			//!	Initialize the ARToolKit module
+			/*!
+				Initializes the ARToolKit library and prepares the capture system.
+
+				Also sets the transformation matrix to be modified when the pattern is detected.
+
+				/param	glTransformationMatrix	Pointer to the shared transformation matrix.
+			*/
 			void init(float *glTransformationMatrix);
 
-			// Starts marker recognition
+			//!	Starts the application main loop
+			/*!
+				Calls the main loop of the GLUT library. The ARToolKit capture and recognize loop runs in a GLUT window.
+
+				Every time a pattern is recognized, the shared transformation matrix is set to reflect the pattern positioning, ignoring any translation.
+
+				When no pattern is found, the matrix is set to be the identity.
+			*/
 			void run();
 
+			//!	Ask for access to the output data.
+			/*!
+				Asks for access to the shared transformation matrix, to avoid race conditions.
+			*/
 			void lockOutput();
+
+			//!	Dismiss the access to the output data.
+			/*!
+				Releases the shared transformation matrix, avoiding deadlocks.
+			*/
 			void unlockOutput();
 		}
 	}
